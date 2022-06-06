34c247b033bcb2235bec9efb26a38fd2cb9e6f45
Shobana-27 <shobana27seetha@gmail.com>
2022-06-06 06:47:16 +0530

    adding addition and multiplication function

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,15 @@
+from pyrsistent import b
+
+
 print("add all data processing code related fraud detection")
+
+a=5
+b=7
+
+def sum_nums(x,y):
+    z = x+y
+    return z
+
+def multiplication(x,y):
+    z=x*y
+    return z    
